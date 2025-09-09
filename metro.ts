82f2325802182308565568e6190c35d9d6e35fd9
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-09-09 10:33:34 +0200

    fix: enabled in metro

--- metro.ts
+++ metro.ts
@@ -4,13 +4,15 @@ export const withRozeniteReduxDevTools = <T>(
   config: T,
   remoteDevtoolsConfig: ZorroConfig,
 ): T => {
-  import("@redux-devtools/cli").then(({ default: devToolsCli }) => {
-    devToolsCli({
-      ...DEFAULT_CONFIG,
-      ...remoteDevtoolsConfig,
-      logLevel: process.env.ROZENITE_LOG_LEVEL,
+  if (remoteDevtoolsConfig.enabled) {
+    import("@redux-devtools/cli").then(({ default: devToolsCli }) => {
+      devToolsCli({
+        ...DEFAULT_CONFIG,
+        ...remoteDevtoolsConfig,
+        logLevel: process.env.ROZENITE_LOG_LEVEL,
+      });
     });
-  });
+  }
 
   return config;
 };
